1ae3b2aa2d5ca647f644d342aabaf1462952fdfc
zettrix <fibreun@gmail.com>
2018-09-19 23:02:45 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,30 +1,55 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.arr = [];
+
+    this.sorting = function (a,b){
+      return a - b;
+    }
   }
 
   add(element) {
     // your implementation
+    this.arr.push(element)
+
+    return this.arr
   }
 
   at(index) {
     // your implementation
+    return this.arr[index]
   }
 
   get length() {
     // your implementation
+    return this.arr.length
   }
 
   toArray() {
     // your implementation
+    return this.arr
   }
 
   sort(indices) {
     // your implementation
+    var newArr = [],
+        length = indices.length;
+    indices.sort(function(a,b){return a- b});
+    for(var i = 0; i < length; i++){
+      newArr.push(this.arr[indices[i]]);
+    }
+    newArr.sort(this.sorting);
+
+    for(var i = 0; i < length; i++){
+      this.arr[indices[i]] = newArr[i]; 
+    }
+    
+    return this.arr;
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.sorting = compareFunction;
   }
 }
 
